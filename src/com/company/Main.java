c3d699eb1d77ae74cf469065dd04824983565625
eg913 <eg913@pvsd.org>
2018-11-05 11:19:48 -0500

    uggghhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+       //ideas- separate array for name, grade, number of absences?
+        //Print out class average
+        //could use a for loop that sums all of the grades and divides them by the array length
+
+        //Print out number if As, Bs, Cs, Ds, Fs
+        //
+
+        //Search Feature which retrieves student name, average, class rank, number of absences
+
+        //Warning
     }
 }
